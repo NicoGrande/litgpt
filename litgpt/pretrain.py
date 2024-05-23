2ccfa21ba0b0ff1ad73f1bdcb21e2cbd84672e1b
Nicolas Grande <nicogrande@google.com>
2024-05-23 22:39:33 +0000

    debug monitor collectives

--- litgpt/pretrain.py
+++ litgpt/pretrain.py
@@ -253,7 +253,7 @@ def main(
         fabric.print(f"Total parameters: {num_parameters(model):,}")
 
         model = torch.compile(model)
-        model = fabric.setup(model)
+        model = fabric.setup(model, _reapply_compile=False)
 
         optimizer = torch.optim.AdamW(
             model.parameters(),
